c493f7807e69e88bc4c4e9bef0620dd06be78b90
Ayodeji <ayodejiibimilua4@gmail.com>
2021-12-01 06:51:26 +0100

    ssrn and wol api added

--- api/app.py
+++ api/app.py
@@ -151,12 +151,168 @@ class ScienceDirect(Resource):
 
         return(flist)
 
+
+class WOL(Resource):
+
+    #hello_args = {"search_word": fields.Str(required=True)}
+
+    #@use_args(hello_args)
+
+    def get(self):
+
+        browser = Firefox(options = options)
+                
+        browser.get("https://onlinelibrary.wiley.com/")
+
+        browser.implicitly_wait(5)
+
+        search_word = "computer"
+        browser.find_element_by_id("searchField1").send_keys(search_word)
+        browser.find_element_by_xpath("//button[@aria-label='Submit Search']").click()
+        browser.implicitly_wait(5)
+
+        divs = browser.find_elements_by_xpath("//ul[@id='search-result']/li")
+        browser.implicitly_wait(5)
+
+        author = []
+        date = []
+        title = []
+        link = []
+
+        flist = []
+
+        for i in divs:
+            try: 
+                author_tx = i.find_element_by_xpath(".//div[@class='meta__authors comma']").text
+                author.append(author_tx)
+            except:
+                author_tx = ""
+                author.append(author_tx)
+            
+            try: 
+                date_tx = i.find_element_by_xpath(".//p[@class='meta__epubDate']").text
+                
+                date_tx = date_tx.split("First published: ")[1]
+                date.append(date_tx)
+            except:
+                date_tx = ""
+                date.append(date_tx)
+                
+            try: 
+                link_tx = i.find_element_by_xpath(".//div/h2/.//a").get_attribute("href")
+                link.append(link_tx)
+            except:
+                link_tx = ""
+                link.append(link_tx)
+                
+            try: 
+                title_tx = i.find_element_by_xpath(".//div/h2/.//a").text
+                title.append(title_tx)
+            except:
+                title_tx = ""
+                title.append(title_tx)
+
+            pdict = {
+                    "author": author_tx,
+                    "date": date_tx,
+                    "link": link_tx,
+                    "title": title_tx}
+
+            flist.append(pdict)
+
+        browser.close()
+        browser.quit()
+        
+
+        return(flist)
+
+
+class SSRN(Resource):
+
+    #hello_args = {"search_word": fields.Str(required=True)}
+
+    #@use_args(hello_args)
+
+    def get(self):
+
+        browser = Firefox(options = options)
+                
+        browser.get("https://www.ssrn.com/index.cfm/en/")
+        browser.implicitly_wait(5)
+
+
+        search_word = "computer"
+        browser.find_element_by_id("txtKeywords").send_keys(search_word)
+        browser.implicitly_wait(5)
+
+        browser.find_element_by_xpath("//button[@class='primary ']").click()
+        browser.implicitly_wait(5)
+
+
+        divs = browser.find_elements_by_xpath("//div[@class='table results papers-list']/div/div")
+        browser.implicitly_wait(5)
+
+        author = []
+        date = []
+        title = []
+        link = []
+
+        flist = []
+
+        for i in divs:
+            try: 
+                author_tx = i.find_element_by_xpath(".//div[@class='authors-list']").text
+                author.append(author_tx)
+            except:
+                author_tx = ""
+                author.append(author_tx)
+            
+            try: 
+                date_tx = i.find_element_by_xpath(".//div[@class='note note-list']/span[2]").text
+                
+                date_tx = date_tx.split("Posted: ")[1]
+                date.append(date_tx)
+            except:
+                date_tx = ""
+                date.append(date_tx)
+                
+            try: 
+                link_tx = i.find_element_by_xpath(".//div[@class='description']/h3/.//a").get_attribute("href")
+                link.append(link_tx)
+            except:
+                link_tx = ""
+                link.append(link_tx)
+                
+            try: 
+                title_tx = i.find_element_by_xpath(".//div[@class='description']/h3/.//a").text
+                title.append(title_tx)
+            except:
+                title_tx = ""
+                title.append(title_tx)
+
+            pdict = {
+                    "author": author_tx,
+                    "date": date_tx,
+                    "link": link_tx,
+                    "title": title_tx}
+
+            flist.append(pdict)
+
+        browser.close()
+        browser.quit()
+        
+
+        return(flist)
+
 @parser.error_handler
 def handle_request_parsing_error(err, req, schema, *, error_status_code, error_headers):
     abort(error_status_code, errors=err.messages)
 
 api.add_resource(IEEE, '/', '/ieee')
 api.add_resource(ScienceDirect, '/', '/sciencedirect')
+api.add_resource(WOL, '/', '/wol')
+api.add_resource(SSRN, '/', '/ssrn')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
